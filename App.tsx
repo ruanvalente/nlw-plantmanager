f9cbccf69a5156bef502570b4954770013540730
ruanvalente <ruanopensuselinux@gmail.com>
2021-06-22 22:27:47 -0300

    feat: added fonts for app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
+import AppLoading from "expo-app-loading";
+
 import { Welcome } from "./src/pages/Welcome";
-import { UserConfirmation } from './src/pages/UserConfirmation'
+import { UserConfirmation } from "./src/pages/UserConfirmation";
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <UserConfirmation />
+      <Welcome />
     </View>
   );
 }
